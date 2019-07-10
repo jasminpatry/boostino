847d74638a8ec979e2bd25b433d00db58be52c01
Jasmin Patry <jasmin.patry@gmail.com>
2019-07-09 22:23:24 -0700

    Add separate AFR and MPH labels; untested

---
+++ labelAfr.h
@@ -0,0 +1,9 @@
+#ifndef LABELAFR_H
+#define LABELAFR_H
+
+#include "Arduino.h"
+
+extern const uint16_t g_aNBbLabelAfr[];
+extern const uint8_t g_aBLabelAfr[];
+
+#endif // LABELAFR_H
---
+++ labelMph.h
@@ -0,0 +1,9 @@
+#ifndef LABELMPH_H
+#define LABELMPH_H
+
+#include "Arduino.h"
+
+extern const uint16_t g_aNBbLabelMph[];
+extern const uint8_t g_aBLabelMph[];
+
+#endif // LABELMPH_H
